de780eda186e9d2dc59dccab77ade5cc26380fe6
merfais <merfais.bwq@163.com>
2016-05-14 00:51:08 +0800

    AngularJS学习
    指令，控制器，双向数据绑定

--- webBegin/js/angularStudy.js
+++ webBegin/js/angularStudy.js
@@ -85,8 +85,7 @@ userInfoModule.directive("linktest", function() {
 			element.on("mouseenter", function() {
 				//scope.loadData();
 				//scope.$apply("loadData1()");	//对于指令内的事件的绑定和调用
-				
-				
+
 				//使用attribute属性，绑定属性级别的事件，
 				//来实现同一指令完成不同控制器中的函数调用。
 				//每个控制器下的元素，使用属性绑定函数的方式，	attr="func()"
@@ -103,11 +102,11 @@ userInfoModule.directive("linktest", function() {
 //所以一般公用的方法写到controller中，私有的方法写到link中
 userInfoModule.directive("superman", function() {
 	return {
-		scope: {},	//独立的作用域
+		scope: {}, //独立的作用域
 		restrict: "E",
 		controller: function($scope) {
-			$scope.abilities = [];	//暴露外边的属性
-			this.AddSpeed = function() {	//暴露外边的三个方法
+			$scope.abilities = []; //暴露外边的属性
+			this.AddSpeed = function() { //暴露外边的三个方法
 				$scope.abilities.push("speed");
 			};
 			this.AddPower = function() {
@@ -117,19 +116,19 @@ userInfoModule.directive("superman", function() {
 				$scope.abilities.push("light");
 			}
 		},
-		link: function(scope, element, attrs) {	//通常做一些事件的绑定
-			element.on("mouseenter", function() {	//，不公用的方法
+		link: function(scope, element, attrs) { //通常做一些事件的绑定
+			element.on("mouseenter", function() { //，不公用的方法
 				console.log(scope.abilities);
-			}).addClass("btn btn-default");		//增加样式之类的逻辑
+			}).addClass("btn btn-default"); //增加样式之类的逻辑
 		}
 	}
 });
 //依赖指令的使用方式。同过调用依赖的指令的外部方法（controller）来实现指令间的配合
 userInfoModule.directive("speed", function() {
 	return {
-		require: "^superman",	//当出现require这个参数时，即使用依赖的指令，
-		link: function(scope, element, attrs, supermanCtrl) {	//出现第四个参数，依赖指令
-			supermanCtrl.AddSpeed();	//依赖的指令中的controller暴露在外的方法属性均可使用，
+		require: "^superman", //当出现require这个参数时，即使用依赖的指令，
+		link: function(scope, element, attrs, supermanCtrl) { //出现第四个参数，依赖指令
+			supermanCtrl.AddSpeed(); //依赖的指令中的controller暴露在外的方法属性均可使用，
 		}
 	}
 });
@@ -149,3 +148,49 @@ userInfoModule.directive("light", function() {
 		}
 	}
 });
+//独立作用域，独立作用域的数据绑定
+userInfoModule.directive("duli", function() {
+	return {
+		scope: {
+			/*
+			 * 单向数据绑定，
+			 * 绑定的为指令中的属性，和替换文本中的值
+			 * 在控制器中为属性设置值，在替换的文档中展现此值
+			 */
+			dulivalue: "@"
+		},
+		restrict: "E",
+		template: "<div><input type='text' ng-model='dulivalue' />{{dulivalue}}</div>",
+		replace: true,
+		//link: function(scope, element, attrs) {
+		//	scope.dulivalue = attrs.flavor;
+		//}
+	}
+});
+userInfoModule.directive("dulioto", function() {
+	return {
+		scope: {
+			dulivalue1: "=", //双向数据绑定
+		},
+		template: "<div><input type='text' ng-model='dulivalue1' />{{dulivalue1}}</div>",
+		replace: true
+	}
+});
+userInfoModule.directive("greeting", function() {
+	return {
+		restrict: "E",
+		template: "<input type='text' ng-model='userName' />" +
+			"<button class='btn btn-defualt' ng-click='greet({name:userName})'>控制台打印</button><br/>",
+		scope: {
+			greet:"&"
+		}
+	}
+});
+
+userInfoModule.controller("duliCtrl", ["$scope", function($scope) {
+	$scope.flavor = "百威";
+	$scope.flavor1 = "可乐";
+	$scope.sayHello = function(name) {
+		console.log(name);
+	}
+}]);
